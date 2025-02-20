3a38c53e68854cccfc2dc5c2c38100d310833573
fayponjp <fayponjp@gmail.com>
2025-02-20 22:01:00 +0800

    refactored scoring functions

--- index.js
+++ index.js
@@ -7,6 +7,20 @@ const guestScoreElement = document.getElementById("guestScore");
 homeScoreElement.textContent = 0;
 guestScoreElement.textContent = 0;
 
+const buttonsArray = document.querySelectorAll(".scoreButton");
+console.log(buttonsArray);
+
+for(let i = 0; i < buttonsArray.length; i++) {
+    let team = buttonsArray[i].classList.contains("homeButton") ? "home" : "guest";
+    
+    let points = buttonsArray[i].classList.contains("onePoint") ? 1
+    : buttonsArray[i].classList.contains("twoPoint") ? 2
+    : 3;
+
+    buttonsArray[i].addEventListener("click", () => updatePoints(points, team));
+
+}
+
 function updatePoints(points, team) {
     if (team === "home") {
         currentHomeScore += points;
